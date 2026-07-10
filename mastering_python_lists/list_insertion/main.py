99bba3cb21fcb6438e18931cd2981231a071b74c
arnabexternal <>
2026-07-10 09:17:32 +0000

    List insertion

---
+++ mastering_python_lists/list_insertion/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Adding new elements
+travel_wishlist.insert(1,"Budapest")
+travel_wishlist.insert(0,"London")
+
+# Testing 
+print("Updated travel_wishlist:", travel_wishlist)
